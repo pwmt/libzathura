a0600ee47bf142fcaa23057105bc6aed0632ac2a
Moritz Lipp <mlq@pwmt.org>
2012-10-10 10:35:27 +0200

    Add internal.h

---
+++ libzathura/internal.h
@@ -0,0 +1,12 @@
+/* See LICENSE file for license and copyright information */
+
+#ifndef INTERNAL_H
+#define INTERNAL_H
+
+#include "document.h"
+
+struct zathura_page_s {
+  zathura_document_t* document;
+};
+
+#endif /* INTERNAL_H */
