e71b6405a815059f24b43a9a67063b88e499ef0d
Moritz Lipp <mlq@pwmt.org>
2013-12-09 00:06:47 +0100

    Document plugin-api/document

--- libzathura/plugin-api/document.h
+++ libzathura/plugin-api/document.h
@@ -6,6 +6,16 @@
 #include "../error.h"
 #include "../document.h"
 
+/**
+ * Sets the number of pages that the document has.
+ *
+ * @param[in] document The document object
+ * @param[in] number_of_pages The number of pages
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_document_set_number_of_pages(zathura_document_t* document, unsigned int number_of_pages);
 
 #endif /* PLUGIN_API_DOCUMENT_H */
