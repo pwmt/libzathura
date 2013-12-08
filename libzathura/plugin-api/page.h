d13505c6be908118a3bcb588f415975024c42cae
Moritz Lipp <mlq@pwmt.org>
2013-12-09 00:11:59 +0100

    Document plugin-api/page

--- libzathura/plugin-api/page.h
+++ libzathura/plugin-api/page.h
@@ -6,13 +6,100 @@
 #include "../error.h"
 #include "../page.h"
 
+/**
+ * Creates a new page object
+ *
+ * @param[out] page
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_OUT_OF_MEMORY Out of memory
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_page_new(zathura_page_t** page);
+
+/**
+ * Sets the custom data of the page object
+ *
+ * @param[in] page The page object
+ * @param[in] data The custom data
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_page_set_data(zathura_page_t* page, void* data);
+
+/**
+ * Returns the custom data of the page object
+ *
+ * @param[in] page The page object
+ * @param[out] data The custom data
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_page_get_data(zathura_page_t* page, void** data);
+
+/**
+ * Sets the index of the page
+ *
+ * @param[in] page The page object
+ * @param[in] index The index
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_page_set_index(zathura_page_t* page, unsigned int index);
+
+/**
+ * Sets the width of the page
+ *
+ * @param[in] page The page object
+ * @param[in] width The width of the page
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_page_set_width(zathura_page_t* page, unsigned int width);
+
+/**
+ * Sets the height of the page
+ *
+ * @param[in] page The page object
+ * @param[in] height THe height of the page
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_page_set_height(zathura_page_t* page, unsigned int height);
+
+/**
+ * Sets the label of the page
+ *
+ * @param[in] page The page object
+ * @param[in] label The label
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_page_set_label(zathura_page_t* page, const char* label);
+
+/**
+ * Sets the transition object of the page
+ *
+ * @param[in] page The page object
+ * @param[in] transition The transition object
+ *
+ * @return ZATHURA_ERROR_OK No error occurred
+ * @return ZATHURA_ERROR_INVALID_ARGUMENTS Invalid arguments have been passed
+ * @return ZATHURA_ERROR_UNKNOWN An unspecified error occurred
+ */
 zathura_error_t zathura_page_set_transition(zathura_page_t* page, zathura_page_transition_t* transition);
 
 #endif /* PLUGIN_API_PAGE_H */
