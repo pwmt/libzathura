7c2c304db7abae190d50a9119e5306c03959df0f
Moritz Lipp <mlq@pwmt.org>
2014-10-03 12:22:06 +0200

    Call correct functions in circle test cases

--- tests/annotations/annotation-circle.c
+++ tests/annotations/annotation-circle.c
@@ -22,10 +22,10 @@ START_TEST(test_annotation_circle_set_rectangle) {
   zathura_rectangle_t rectangle = { {0, 0}, {0, 0} };
 
   /* invalid arguments */
-  fail_unless(zathura_annotation_square_set_rectangle(NULL, rectangle) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_set_rectangle(NULL, rectangle) == ZATHURA_ERROR_INVALID_ARGUMENTS);
 
   /* valid arguments */
-  fail_unless(zathura_annotation_square_set_rectangle(annotation, rectangle) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_set_rectangle(annotation, rectangle) == ZATHURA_ERROR_OK);
 } END_TEST
 
 START_TEST(test_annotation_circle_get_rectangle) {
@@ -33,19 +33,19 @@ START_TEST(test_annotation_circle_get_rectangle) {
   zathura_rectangle_t rectangle_input = { {1, 2}, {3, 4} };
 
   /* invalid arguments */
-  fail_unless(zathura_annotation_square_get_rectangle(NULL, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
-  fail_unless(zathura_annotation_square_get_rectangle(annotation, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
-  fail_unless(zathura_annotation_square_get_rectangle(NULL, &rectangle) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_rectangle(NULL, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_rectangle(annotation, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_rectangle(NULL, &rectangle) == ZATHURA_ERROR_INVALID_ARGUMENTS);
 
   /* valid arguments */
-  fail_unless(zathura_annotation_square_get_rectangle(annotation, &rectangle) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_get_rectangle(annotation, &rectangle) == ZATHURA_ERROR_OK);
   fail_unless(rectangle.p1.x == 0);
   fail_unless(rectangle.p1.y == 0);
   fail_unless(rectangle.p2.x == 0);
   fail_unless(rectangle.p2.y == 0);
 
-  fail_unless(zathura_annotation_square_set_rectangle(annotation, rectangle_input) == ZATHURA_ERROR_OK);
-  fail_unless(zathura_annotation_square_get_rectangle(annotation, &rectangle) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_set_rectangle(annotation, rectangle_input) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_get_rectangle(annotation, &rectangle) == ZATHURA_ERROR_OK);
   fail_unless(rectangle.p1.x == 1);
   fail_unless(rectangle.p1.y == 2);
   fail_unless(rectangle.p2.x == 3);
@@ -56,10 +56,10 @@ START_TEST(test_annotation_circle_set_color) {
   zathura_annotation_color_t color = { 0 };
 
   /* invalid arguments */
-  fail_unless(zathura_annotation_square_set_color(NULL, color) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_set_color(NULL, color) == ZATHURA_ERROR_INVALID_ARGUMENTS);
 
   /* valid arguments */
-  fail_unless(zathura_annotation_square_set_color(annotation, color) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_set_color(annotation, color) == ZATHURA_ERROR_OK);
 } END_TEST
 
 START_TEST(test_annotation_circle_get_color) {
@@ -67,15 +67,15 @@ START_TEST(test_annotation_circle_get_color) {
   zathura_annotation_color_t color_input = { ZATHURA_ANNOTATION_COLOR_SPACE_NONE, {1, 2, 3, 4} };
 
   /* invalid arguments */
-  fail_unless(zathura_annotation_square_get_color(NULL, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
-  fail_unless(zathura_annotation_square_get_color(annotation, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
-  fail_unless(zathura_annotation_square_get_color(NULL, &color) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_color(NULL, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_color(annotation, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_color(NULL, &color) == ZATHURA_ERROR_INVALID_ARGUMENTS);
 
   /* valid arguments */
-  fail_unless(zathura_annotation_square_get_color(annotation, &color) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_get_color(annotation, &color) == ZATHURA_ERROR_OK);
 
-  fail_unless(zathura_annotation_square_set_color(annotation, color_input) == ZATHURA_ERROR_OK);
-  fail_unless(zathura_annotation_square_get_color(annotation, &color) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_set_color(annotation, color_input) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_get_color(annotation, &color) == ZATHURA_ERROR_OK);
 } END_TEST
 
 START_TEST(test_annotation_circle_set_border) {
@@ -84,10 +84,10 @@ START_TEST(test_annotation_circle_set_border) {
     ZATHURA_ANNOTATION_BORDER_EFFECT_NONE, 0.0 };
 
   /* invalid arguments */
-  fail_unless(zathura_annotation_square_set_border(NULL, border) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_set_border(NULL, border) == ZATHURA_ERROR_INVALID_ARGUMENTS);
 
   /* valid arguments */
-  fail_unless(zathura_annotation_square_set_border(annotation, border) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_set_border(annotation, border) == ZATHURA_ERROR_OK);
 } END_TEST
 
 START_TEST(test_annotation_circle_get_border) {
@@ -99,13 +99,13 @@ START_TEST(test_annotation_circle_get_border) {
     ZATHURA_ANNOTATION_BORDER_EFFECT_NONE, 0.0 };
 
   /* invalid arguments */
-  fail_unless(zathura_annotation_square_get_border(NULL, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
-  fail_unless(zathura_annotation_square_get_border(annotation, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
-  fail_unless(zathura_annotation_square_get_border(NULL, &border) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_border(NULL, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_border(annotation, NULL) == ZATHURA_ERROR_INVALID_ARGUMENTS);
+  fail_unless(zathura_annotation_circle_get_border(NULL, &border) == ZATHURA_ERROR_INVALID_ARGUMENTS);
 
   /* valid arguments */
-  fail_unless(zathura_annotation_square_get_border(annotation, &border) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_get_border(annotation, &border) == ZATHURA_ERROR_OK);
 
-  fail_unless(zathura_annotation_square_set_border(annotation, border_input) == ZATHURA_ERROR_OK);
-  fail_unless(zathura_annotation_square_get_border(annotation, &border) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_set_border(annotation, border_input) == ZATHURA_ERROR_OK);
+  fail_unless(zathura_annotation_circle_get_border(annotation, &border) == ZATHURA_ERROR_OK);
 } END_TEST
