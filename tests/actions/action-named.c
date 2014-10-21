7f48480d1dfaf934c09a3faaa1f2a6504aa5ef9d
Moritz Lipp <mlq@pwmt.org>
2014-10-21 14:37:19 +0200

    Implement and test named action

---
+++ tests/actions/action-named.c
@@ -0,0 +1,45 @@
+/* See LICENSE file for license and copyright information */
+
+#include <check.h>
+
+#include "action.h"
+
+static void setup_action_named(void) {
+  fail_unless(zathura_action_new(&action, ZATHURA_ACTION_NAMED) == ZATHURA_ERROR_OK);
+  fail_unless(action != NULL);
+}
+
+START_TEST(test_action_named_new) {
+} END_TEST
+
+START_TEST(test_action_named_get_type) {
+  zathura_action_type_t type;
+  fail_unless(zathura_action_get_type(action, &type) == ZATHURA_ERROR_OK);
+  fail_unless(type == ZATHURA_ACTION_NAMED);
+} END_TEST
+
+START_TEST(test_action_named_init) {
+  /* invalid arguments */
+  fail_unless(zathura_action_named_init(NULL)
+      == ZATHURA_ERROR_INVALID_ARGUMENTS);
+
+  /* valid arguments */
+  fail_unless(zathura_action_named_init(action)
+      == ZATHURA_ERROR_OK); // double initialization
+
+  /* fault injection */
+  fiu_enable("libc/mm/calloc", 1, NULL, 0);
+  fail_unless(zathura_action_named_init(action)
+      == ZATHURA_ERROR_OUT_OF_MEMORY); // double initialization
+  fiu_disable("libc/mm/calloc");
+} END_TEST
+
+START_TEST(test_action_named_clear) {
+  /* invalid arguments */
+  fail_unless(zathura_action_named_clear(NULL)
+      == ZATHURA_ERROR_INVALID_ARGUMENTS);
+
+  /* valid arguments */
+  fail_unless(zathura_action_named_clear(action)
+      == ZATHURA_ERROR_OK);
+} END_TEST
