0d79b57e22cd1e6ee11b3942a1ddead135ea6744
Moritz Lipp <mlq@pwmt.org>
2014-10-21 14:37:53 +0200

    Implement and set-ocg-state action

---
+++ libzathura/actions/action-set-ocg-state.c
@@ -0,0 +1,54 @@
+/* See LICENSE file for license and copyright information */
+
+#include <stdlib.h>
+
+#include "../action.h"
+#include "../error.h"
+
+#include "action-set-ocg-state.h"
+
+#define ACTION_SET_OCG_STATE_CHECK_TYPE() \
+  if (action->type != ZATHURA_ACTION_SET_OCG_STATE) { \
+    return ZATHURA_ERROR_ACTION_INVALID_TYPE; \
+  }
+
+#define ACTION_SET_OCG_STATE_CHECK_DATA() \
+  if (action->data.set_ocg_state_dest == NULL) { \
+    return ZATHURA_ERROR_UNKNOWN; \
+  }
+
+zathura_error_t
+zathura_action_set_ocg_state_init(zathura_action_t* action)
+{
+  if (action == NULL) {
+    return ZATHURA_ERROR_INVALID_ARGUMENTS;
+  }
+
+  ACTION_SET_OCG_STATE_CHECK_TYPE()
+
+  if (action->data.set_ocg_state_dest != NULL) {
+    free(action->data.set_ocg_state_dest);
+  }
+
+  action->data.set_ocg_state_dest = calloc(1, sizeof(zathura_action_set_ocg_state_t));
+  if (action->data.set_ocg_state_dest == NULL) {
+    return ZATHURA_ERROR_OUT_OF_MEMORY;
+  }
+
+  return ZATHURA_ERROR_OK;
+}
+
+zathura_error_t
+zathura_action_set_ocg_state_clear(zathura_action_t* action)
+{
+  if (action == NULL) {
+    return ZATHURA_ERROR_INVALID_ARGUMENTS;
+  }
+
+  ACTION_SET_OCG_STATE_CHECK_TYPE()
+
+  free(action->data.set_ocg_state_dest);
+  action->data.set_ocg_state_dest = NULL;
+
+  return ZATHURA_ERROR_OK;
+}
