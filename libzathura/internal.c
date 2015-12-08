fd192ffda172004c657a237834fadb6eea2ea5a8
Sebastian Ramacher <sebastian+dev@ramacher.at>
2015-12-09 00:09:18 +0100

    Fix typo
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- libzathura/internal.c
+++ libzathura/internal.c
@@ -1,7 +1,7 @@
 /* See LICENSE file for license and copyright information */
 
 #define _DEFAULT_SOURCE
-#define _XOPEN_SOUCE 500
+#define _XOPEN_SOURCE 500
 
 #include <stdlib.h>
 #include <string.h>
