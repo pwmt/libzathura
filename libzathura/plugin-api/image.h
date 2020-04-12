8135a4e527c3bd1d1cea16d93917441450f17f4d
Moritz Lipp <mail@mlq.me>
2020-04-12 22:37:23 +0200

    Check for cairo

--- libzathura/plugin-api/image.h
+++ libzathura/plugin-api/image.h
@@ -11,7 +11,9 @@ extern "C" {
 #include "../page.h"
 #include "../types.h"
 
+#ifdef HAVE_CAIRO
 typedef zathura_error_t (*zathura_image_get_cairo_surface_t)(zathura_image_t* image, cairo_surface_t** surface);
+#endif
 typedef zathura_error_t (*zathura_image_get_buffer_t)(zathura_image_t* image, zathura_image_buffer_t** buffer);
 
 zathura_error_t zathura_image_new(zathura_image_t** image, zathura_rectangle_t position);
@@ -20,7 +22,9 @@ zathura_error_t zathura_image_set_user_data(zathura_image_t* image, void* user_d
 
 zathura_error_t zathura_image_get_user_data(zathura_image_t* image, void** user_data);
 
+#ifdef HAVE_CAIRO
 zathura_error_t zathura_image_set_get_cairo_surface_function(zathura_image_t* image, zathura_image_get_cairo_surface_t function);
+#endif
 
 zathura_error_t zathura_image_set_get_buffer_function(zathura_image_t* image, zathura_image_get_buffer_t function);
 
